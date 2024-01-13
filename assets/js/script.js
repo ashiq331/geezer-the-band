1d7673123aa7617083c0ea522166038213284789
ashiq331 <ashiq33030@gmail.com>
2024-01-13 23:42:37 +0600

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 let topbutton = document.getElementById("myBtn");
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
