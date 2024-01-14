c40770305e768614b638796b9430fa28d3e16a6b
ashiq331 <ashiq33030@gmail.com>
2024-01-14 14:39:35 +0600

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,3 @@
-/* magnificPopup video view */
-$('.popup-video').magnificPopup({
-	type: 'iframe'
-});
-
-
 // Scroll to top
 let topbutton = document.getElementById("myBtn");
 
@@ -23,3 +17,27 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+(function ($) {
+  "use strict";
+
+  $('.popup-youtube').magnificPopup({
+    // disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false
+  });
+
+
+ $('.gallery_img').magnificPopup({
+  type: 'image',
+  gallery:{
+    enabled:true
+  }
+});
+
+}(jQuery));
+
